af136ab03fd74b92b11ed1708fa14fafe8f95fd4
Tori Hedden <tori.hedden@gmail.com>
2016-12-18 14:01:23 -0500

    Style, add JS for randomization of button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,30 @@
-console.log("scripts file here");
+var x;
+var y;
 
-$(".button").click(function() {
+function randomInt() {
 
-})
+  x = Math.floor((Math.random() * 100)) + 1;
+
+  y = Math.floor((Math.random() * 100)) + 1;
+
+  console.log(x, y);
+}
+
+randomInt();
+
+
+$("button").click(function() {
+
+  // $(".submit-button").css({"top" : "calc( 100% / 10)"});
+  // $(".submit-button").css({"left" : "calc( 100% / 2 )"});
+
+  $(".submit-button").css({"top" : x + "%"});
+  $(".submit-button").css({"left" : y + "%"});
+
+  randomInt();
+
+});
+
+
+
+// another form idea: on click submit, erase contents of one random input element, and prompt user
